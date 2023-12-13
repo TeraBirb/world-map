84157a4634523067011af022fdfe56d358bfff17
Isaiah <isaiah.g.callano@gmail.com>
2023-12-13 15:59:34 -0800

    generated http service

---
+++ src/app/http.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private url = 'https://api.worldbank.org/V2/country/';
+
+  constructor(private http: HttpClient) { }
+
+  callAPI(countryId: string) {
+    return this.http.get(this.url + countryId);
+  }
+}
